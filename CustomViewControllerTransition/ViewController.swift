24b0d73981ebe2303e906e2c8b3142c305498adf
Christian Vershkov <christian.vershkov@altexsoft.com>
2019-04-23 12:27:12 +0300

    Initial Commit

---
+++ CustomViewControllerTransition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomViewControllerTransition
+//
+//  Created by Christian Vershkov on 4/23/19.
+//  Copyright © 2019 Christian Vershkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
